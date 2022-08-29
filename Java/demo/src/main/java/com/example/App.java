ee692cae31c032c329717f449dbdb3a59d5dd30f
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2022-08-29 18:55:12 +0300

    initial commit

---
+++ Java/demo/src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class App{
+    public static void main( String[] args ){
+        String myStr = "Hello WORLD!";
+        for (int i = 0; i < 10; i++) {
+            System.out.println(myStr);
+        }
+    }
+}
