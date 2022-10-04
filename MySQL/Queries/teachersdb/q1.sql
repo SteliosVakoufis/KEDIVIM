7f93822019d517377dc459d3624e55b6ad0323b7
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2022-10-04 12:26:26 +0300

    MySQL query scripts initial commit

---
+++ MySQL/Queries/teachersdb/q1.sql
@@ -0,0 +1,7 @@
+USE teachersdb;
+
+INSERT INTO students(LASTNAME, FIRSTNAME) VALUES ('Snow', 'John');
+INSERT INTO students(LASTNAME, FIRSTNAME) VALUES ('Karas', 'George');
+INSERT INTO students(LASTNAME, FIRSTNAME) VALUES ('Androutsos', 'Kostas');
+
+SELECT * FROM students;
