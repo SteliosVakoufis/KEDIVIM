7f4c9a82a1636ecf2720bc798df8d3ceeeee61b3
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2022-10-04 16:56:17 +0300

    MySQL exercises

--- MySQL/Queries/teachersdb/q1.sql
+++ MySQL/Queries/teachersdb/q1.sql
@@ -5,3 +5,12 @@ INSERT INTO students(LASTNAME, FIRSTNAME) VALUES ('Karas', 'George');
 INSERT INTO students(LASTNAME, FIRSTNAME) VALUES ('Androutsos', 'Kostas');
 
 SELECT * FROM students;
+
+SELECT s.ID
+	FROM students AS s
+    WHERE s.LASTNAME = 'Snow'
+;
+
+SELECT COUNT(*) AS "Number of Students"
+	FROM students
+;
