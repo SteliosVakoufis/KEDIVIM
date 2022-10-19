0a1366ab2216e4d3c525789ee579437a5c322161
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2022-10-19 23:23:15 +0300

    small csharp test

---
+++ Csharp/TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckType<MyClass1>();
+        }
+
+        private static void CheckType<T>()
+        {
+            Console.WriteLine(typeof(T));
+            if (typeof(T) is MyClass1)
+            {
+                Console.WriteLine("It's class1");
+            }
+            else if (typeof(T) is MyClass2)
+            {
+                Console.WriteLine("It's class2");
+            }
+            else
+            {
+                Console.WriteLine("Unkown Type");
+            }
+        }
+    }
+}
